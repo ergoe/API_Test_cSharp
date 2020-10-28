d933adec021790e3059a99a14785fffba8a6f7f9
Eric Goeckeritz <ergoe@icloud.com>
2020-10-28 18:40:05 -0400

    Turning off MongoDb Connection for test

--- Hooks.cs
+++ Hooks.cs
@@ -89,7 +89,8 @@ namespace Api_TestFramework
             //Attach report to reporter
             extent = new AventStack.ExtentReports.ExtentReports();
             klov = new ExtentKlovReporter();
-            klov.InitMongoDbConnection("localhost", 27017);
+            //klov.InitMongoDbConnection("localhost", 27017);
+            //klov.InitMongoDbConnection("10.100.100.187", 27017);
 
             klov.ProjectName = "APIAutomation Test";
             klov.InitKlovServerConnection("http://localhost:80");
@@ -97,7 +98,7 @@ namespace Api_TestFramework
             klov.ReportName = "Karthik KK" + System.DateTime.Now.ToString();
 
             extent.AttachReporter(htmlReporter);
-            extent.AttachReporter(klov);
+            //extent.AttachReporter(klov);
             
         }
 
